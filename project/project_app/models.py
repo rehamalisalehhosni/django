707ed79cfe4ba952ee3431f47bef94efe4edf646
homa <engreham.ali2011@gmail.com>
2016-03-22 15:11:33 +0200

    DB

--- project/project_app/models.py
+++ project/project_app/models.py
@@ -1,5 +1,35 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.admin import User
 from django.db import models
 
 # Create your models here.
+class Users (User):
+	#name=models.CharField(max_length=255)
+	#email=models.EmailField()
+	#password=models.IntegerField(max_length=32)
+	signup=models.DateField()
+	
+class Categories (models.Model):
+	category_name=models.CharField(max_length=255)
+
+class Country (models.Model):
+	country_name=models.CharField(max_length=255)
+class City (models.Model):
+	city_name=models.CharField(max_length=255)
+	coun_id=models.ForeignKey(Country);
+class Property (models.Model):
+	prop_name=models.CharField(max_length=255)
+	uid=models.ForeignKey(Users);
+	cat_id=models.ForeignKey(Categories);
+	city_id=models.ForeignKey(City);
+	address=models.CharField(max_length=255)
+	youtube=models.URLField(max_length=255)
+	phone=models.IntegerField()
+	owner=models.CharField(max_length=255)
+	area=models.FloatField()
+	price=models.FloatField()
+	preview=models.TextField()
+	details=models.TextField()
+	longtiude=models.FloatField()
+	Latitude=models.FloatField()
+
